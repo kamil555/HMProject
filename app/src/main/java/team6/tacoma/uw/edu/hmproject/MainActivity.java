c2fd8a59beacb8dfd6df2be60775bb5e64ece913
kamil555 <kamil555111@hotmail.com>
2016-05-01 16:51:55 -0700

    commit

--- app/src/main/java/team6/tacoma/uw/edu/hmproject/MainActivity.java
+++ app/src/main/java/team6/tacoma/uw/edu/hmproject/MainActivity.java
@@ -1,13 +1,33 @@
 package team6.tacoma.uw.edu.hmproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        // Now get a handle to any View contained
+        // within the main layout you are using
+        View view = findViewById(R.id.button_View);
+
+        View root = view.getRootView();
+        root.setBackgroundColor(getResources().getColor(R.color.mainBackground));
+
+        Button button_View = (Button)findViewById(R.id.button_View);
     }
+    //View All button
+    public void button_View(View view) {
+        Intent intent = new Intent(this, ViewAll.class);
+        startActivity(intent);
+    }
+
 }
