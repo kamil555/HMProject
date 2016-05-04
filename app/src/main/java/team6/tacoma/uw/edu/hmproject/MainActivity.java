030c907ca3346ba81ece46632b1fa2f0d06776f8
Han Wang <kamil555111@hotmail.com>
2016-05-04 12:11:23 -0700

    commit

--- app/src/main/java/team6/tacoma/uw/edu/hmproject/MainActivity.java
+++ app/src/main/java/team6/tacoma/uw/edu/hmproject/MainActivity.java
@@ -1,10 +1,12 @@
 package team6.tacoma.uw.edu.hmproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 //        View root = view.getRootView();
 //        root.setBackgroundColor(getResources().getColor(R.color.mainBackground));
 
-        Button button_View = (Button)findViewById(R.id.button_View);
+        Button button_View = (Button) findViewById(R.id.button_View);
         button_View.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,8 +32,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        TextView tev = (TextView) findViewById(R.id.txthelp);
+        tev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://developer.android.com/index.html"));
+                startActivity(intent);
+            }
+        });
     }
-
-
 }
