c6bba2e5fc4b53ac152ec1087f8de379e6121fca
Han Wang <kamil555111@hotmail.com>
2016-05-30 08:37:10 -0700

    1. fixed share error. 2. add new icon and picture on Login, Register, Main Activity. 3. fixed some bugs

--- app/src/main/java/team6/tacoma/uw/edu/hmproject/MainActivity.java
+++ app/src/main/java/team6/tacoma/uw/edu/hmproject/MainActivity.java
@@ -121,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
     public void shareFuction() {
         Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
         sharingIntent.setType("text/plain");
-        String shareBody = "This app is really cool, get it in Google Play Store";
+        String shareBody = "This app is really cool, get it in Google Play Store(Find My Book)";
         sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
         sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
         startActivity(Intent.createChooser(sharingIntent, "Share via"));
